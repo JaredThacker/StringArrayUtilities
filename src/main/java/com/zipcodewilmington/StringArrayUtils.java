19033c4b7acc30f21ccc89a0d1e407315f7516d7
Jared <jared@Zipcoders-MacBook-Pro-3.local>
2024-07-02 23:08:29 -0400

    finished

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,9 +1,6 @@
 package com.zipcodewilmington;
 
-import java.sql.Array;
 import java.util.*;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 /**
  * Created by leon on 1/29/18.
@@ -33,7 +30,7 @@ public class StringArrayUtils {
      */ // TODO
     public static String getLastElement(String[] array) {
 
-        return array[array.length-1];
+        return array[array.length - 1];
     }
 
     /**
@@ -41,7 +38,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return array[array.length-2];
+        return array[array.length - 2];
     }
 
     /**
@@ -82,25 +79,18 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-
-
         String joined = String.join(" ", array);
         String joinedLower = joined.toUpperCase();
-
         boolean isPangram = true;
         int charCount = 0;
-
-        for (int i = 65; i<= 90; i++){
-
-            if (!joinedLower.contains(Character.toString((char)i))){
+        for (int i = 65; i <= 90; i++) {
+            if (!joinedLower.contains(Character.toString((char) i))) {
                 isPangram = false;
                 charCount++;
-            } else if(charCount == 26) {
+            } else if (charCount == 26) {
                 isPangram = true;
             }
-
         }
-
         return isPangram;
     }
 
@@ -113,8 +103,8 @@ public class StringArrayUtils {
 
         int occurences = 0;
 
-        for(String s : array){
-            if(s.contains(value)){
+        for (String s : array) {
+            if (s.contains(value)) {
                 occurences++;
             }
         }
@@ -145,15 +135,15 @@ public class StringArrayUtils {
         ArrayList<String> removedClones = new ArrayList<>(Arrays.asList(array));
         ArrayList<Integer> duplicateIndexes = new ArrayList<>();
 
-        for(int i = 0; i< array.length - 1; i++){
-            if(array[i].equals(array[i+1])) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].equals(array[i + 1])) {
                 duplicateIndexes.add(i + 1);
             }
         }
 
         Collections.reverse(duplicateIndexes);
 
-        for(int index: duplicateIndexes){
+        for (int index : duplicateIndexes) {
             removedClones.remove(index);
         }
 
@@ -172,24 +162,18 @@ public class StringArrayUtils {
 
         for (String eachStr : array) {
             if (accum.isEmpty()) {
-
                 accum = eachStr;
             } else {
                 if (accum.charAt(0) == eachStr.charAt(0)) {
-
                     accum += eachStr;
                 } else {
-
                     consecutiveDupe.add(accum);
-
                     accum = eachStr;
                 }
             }
         }
 
         consecutiveDupe.add(accum);
-
         return consecutiveDupe.toArray(String[]::new);
-
     }
 }
